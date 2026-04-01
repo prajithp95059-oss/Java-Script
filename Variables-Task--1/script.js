4bd7bb143ff422ac17b64aeff9e81e89adcc37d3
prajithp95059-oss <prajithp95059@gmail.com>
2026-04-01 16:20:28 +0530

    //add

---
+++ Variables-Task--1/script.js
@@ -0,0 +1,214 @@
+//1
+var a = 10;
+a = 20;
+var a = 30;
+console.log(a); //Answer:30
+
+//2
+var a = 5;
+var a = 15;
+console.log(a); //Answer:15
+
+//3
+var a = 1;
+a = 1;
+a = 3;
+console.log(a); //Answer:3
+
+//4
+var a = 7;
+a = 14;
+var a = 21;
+a = 28;
+console.log(a); //Answer:28
+
+//5
+var a = 100;
+var a = 200;
+a = 300;
+console.log(a); //Answer:300
+
+//6
+var a = 9;
+console.log(a); //Answer:9
+
+//7
+var a = 11;
+a = 22;
+console.log(a); //Answer:22
+
+//8
+var a = 50;
+var a = 60;
+var a = 70;
+console.log(a) //Answer:70
+
+//9
+var a = 2;
+a = 4;
+a = 6;
+var a = 8;
+console.log(a) //Answer:8
+
+//10
+var a = 99;
+a = 88;
+console.log(a); //Answer:88
+
+//11
+let b = 10;
+b = 20;
+console.log(b); //Answer:20
+
+//12
+let b = 5;
+b = 15;
+b = 25;
+console.log(b); //Answer: 25
+
+//13
+let b =1;
+console.log(b); //Answer: 1
+
+//14
+let b = 7;
+b = 14;
+console.log(b); //Answer:14
+
+//15
+let b = 100;
+b = 100;
+b =300;
+console.log(b) //Answer:300
+
+//16
+let b = 9;
+let b =18;
+console.log(b) //Answer:Syntax error because the variable is already declared.
+
+//17
+let b = 50;
+b = 60;
+let b = 70;
+console.log(b); //Answer:Syntax error because the variable is already declared.
+
+//18
+let b = 2;
+b = 4;
+console.log(b); //Answer:4
+
+//19
+let b =99;
+b = 88;
+b = 77;
+console.log(b); //Answer:77
+
+//20
+let b = 11;
+b = 22;
+console.log(b);  //Answer:22
+
+//21
+const c = 10;
+console.log(c); //Answer:10
+
+//22
+const c = 5;
+c = 15;
+console.log(c); //Answer:Type Error Because c is constant we cant able to reuse or redeclare.
+
+//23
+const c = 1;
+const c = 2;
+console.log(c); //Answer: Syntax error because c is constant we cant able to redeclare.
+
+//24
+const c = 7;
+console.log(c); //Answer:7
+
+//25
+const c = 100;
+console.log(c); //Answer:100
+
+//26
+const c = 50;
+c = 60;
+console.log(c); //Answer:Type Error
+
+//27
+const c = 9;
+console.log(c); //Answer:9
+
+//28
+const c =20;
+const c =30;
+console.log(c); //Answer:Syntax Error
+
+//29
+const c = 99;
+console.log(c); //Answer:99
+
+//30
+const c = 11;
+c = 22;
+console.log(c); //Answer: Type Error
+
+//31
+var a = 1;
+var a = 2;
+var a = 3;
+console.log(a) //Answer:3
+
+//32
+var a = 10;
+a = 20;
+a = 30;
+var a = 40;
+console.log(a) //Answer:40
+
+//33
+var a = 5;
+a = 15;
+console.log(a); //Answer:15
+
+//34
+var a = 100;
+var a = 200;
+var a = 300;
+a = 400;
+console.log(a); //Answer:400
+
+//35
+var a = 7;
+console.log(a) //Answer:7
+
+//36
+let b = 3;
+b = 6;
+b = 9;
+console.log(b); // Answer:9
+
+//37
+let  b = 12;
+let b = 24;
+console.log(b); //Answer:Syntax Error
+
+//38
+let b = 8;
+b = 16;
+console.log(b); //Answer:16
+
+//39
+const c = 2;
+console.log(c); //Answer:2
+
+//40
+const c = 4;
+c = 8; //Answer:Type Error
+
+
+
+
+
+
+
